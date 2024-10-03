5a5a0b25de5832591c43b6f026e49de048b5fe66
yambeni <yambenimirriam@gmail.com>
2024-10-03 16:22:28 +0200

    Set up & Navbar

---
+++ src/app/dashboardWrapper.tsx
@@ -0,0 +1,21 @@
+import Navbar from './(components)/Navbar';
+
+const DashboardWrapper = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
+      {/* side bar */}
+      side bar
+      <main className='flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64'>
+        {/* nav bar */}
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DashboardWrapper;
